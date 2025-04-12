2039677d333316505fdbf9c99606d45798e1566e
Cristian15300 <cguzm721@gmail.com>
2025-04-11 19:49:13 -1200

    Funciones

---
+++ funciones.py
@@ -0,0 +1,17 @@
+def resta (a,b):
+    return a-b
+def multiplicacion(a:int,b:int)-> int:
+    return a*b
+def saludo (a:str) -> str:
+ x = "Hola" + a
+ return x
+def mensaje (x):
+ return("ALERTA:", x)
+
+def despedida (a:str) -> None:
+ x= "Chao conchatumare" + a 
+ print(x)
+
+print (resta(5,2))
+
+print(multiplicacion(5,2))
